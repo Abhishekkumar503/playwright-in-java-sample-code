49c870c52e14c5fb587ed32b50ec72a42cbfe323
Abhishekkumar503 <ak204479@gmail.com>
2025-12-09 02:01:47 +0530

    First testcase to check the page title

--- src/test/java/com/serenitydojo/playwright/ASimplePlaywrightTest.java
+++ src/test/java/com/serenitydojo/playwright/ASimplePlaywrightTest.java
@@ -1,17 +1,39 @@
 package com.serenitydojo.playwright;
 
 import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.asserts.Assertion;
+
+import static java.rmi.server.LogStream.log;
 
 
 public class ASimplePlaywrightTest {
+
+    private static final Logger log = LoggerFactory.getLogger(ASimplePlaywrightTest.class);
 
     @Test
     void shouldShowThePageTitle() {
         // TODO: Write your first playwright test here
 
-        System.out.println(" TEST ");
+        Playwright playwright = Playwright.create();
+
+//        This is for headless browser
+//        Browser browser =  playwright.chromium().launch();
+
+//        This used for UI ( false to headless browser )
+        Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        Page page = browser.newPage();
+        page.navigate("https://www.google.com/");
+        System.out.println(page.title());
+        Assert.assertEquals(page.title(),"Google");
+
+        browser.close();
+        playwright.close();
     }
 }
