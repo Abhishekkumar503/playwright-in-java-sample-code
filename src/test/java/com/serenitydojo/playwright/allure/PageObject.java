93dec782c3b5ce6830fb9be79685ed92d31e9933
Abhishekkumar503 <ak204479@gmail.com>
2026-03-29 03:43:12 +0530

    Fixed Allure Report Generations and change storage path

--- src/test/java/com/serenitydojo/playwright/allure/PageObject.java
+++ src/test/java/com/serenitydojo/playwright/allure/PageObject.java
@@ -75,7 +75,7 @@ public class PageObject implements Screenshot{
         List<CartLineItem> lineItems = checkoutCart.getLineItems();
 
         Assertions.assertThat(lineItems)
-                .hasSize(16)
+                .hasSize(1)
                 .first()
                 .satisfies(item -> {
                     Assertions.assertThat(item.title()).contains("Combination Pliers");
