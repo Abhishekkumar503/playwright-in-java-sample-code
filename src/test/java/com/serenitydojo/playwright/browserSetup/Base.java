0cea0adb0c3f67df31e954da0c9d0c8078297398
Abhishekkumar503 <ak204479@gmail.com>
2026-03-29 04:34:05 +0530

    Final Action setup with Headless browser

--- src/test/java/com/serenitydojo/playwright/browserSetup/Base.java
+++ src/test/java/com/serenitydojo/playwright/browserSetup/Base.java
@@ -12,7 +12,7 @@ public class Base implements OptionsFactory {
     public Options getOptions() {
         return new Options().setLaunchOptions(new BrowserType.
                 LaunchOptions().
-                setHeadless(false).
+                setHeadless(true).
                 setArgs(Arrays.asList("--no-sandbox",
                         "--disable-gpu",
                         "--disable-extensions","--start-maximized"))
